0a3c4c79f4144f2f844fb9b0ca853c6719a65f3a
ivanatodorovic <ititodorovic1998@gmail.com>
2018-03-18 20:01:11 +0100

    Napravljene su hashCode, equals i toString metode za klase Valuta i Kurs. U klasi Kurs umesto liste kurseva sada imamo samo kurs.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -6,7 +6,7 @@ public class Valuta {
 
 private String naziv;
 private String skraceniNaziv;
-private LinkedList<Kurs> kurs = new LinkedList<Kurs>();
+private Kurs kurs;
 
 public String getNaziv() {
 	return naziv;
@@ -20,12 +20,55 @@ public String getSkraceniNaziv() {
 public void setSkraceniNaziv(String skraceniNaziv) {
 	this.skraceniNaziv = skraceniNaziv;
 }
-public LinkedList<Kurs> getKurs() {
+public Kurs getKurs() {
 	return kurs;
 }
-public void setKurs(LinkedList<Kurs> kurs) {
+public void setKurs(Kurs kurs) {
 	this.kurs = kurs;
 }	
 	
 	
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+	result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+	result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Valuta other = (Valuta) obj;
+	if (kurs == null) {
+		if (other.kurs != null)
+			return false;
+	} else if (!kurs.equals(other.kurs))
+		return false;
+	if (naziv == null) {
+		if (other.naziv != null)
+			return false;
+	} else if (!naziv.equals(other.naziv))
+		return false;
+	if (skraceniNaziv == null) {
+		if (other.skraceniNaziv != null)
+			return false;
+	} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+		return false;
+	return true;
+}
+
+
+@Override
+	public String toString() {
+		return "Valuta: " +naziv+" skraceni naziv "+skraceniNaziv +" kurs  [ "+ kurs+"]";
+	}
+
+
 }
