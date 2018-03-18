a399cf85ffd9828ac699de64349b7b1c6cf0883a
ivanatodorovic <ititodorovic1998@gmail.com>
2018-03-18 19:06:25 +0100

    Napravljena je klasa Valuta sa svojim atributima nazivom, skracenim nazivom i kursnom listom. Napravljena je klasa Kurs sa atributom datum.

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,17 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+GregorianCalendar datum;
+
+public GregorianCalendar getDatum() {
+	return datum;
+}
+
+public void setDatum(GregorianCalendar datum) {
+	this.datum = datum;
+}
+	
+
+}
